fe0ac4962a241f289ea6a5977ac7dd9795b3fb40
anajorge <anajessicadegj@gmail.com>
2022-12-08 19:25:17 -0300

    first commit

---
+++ app/src/main/java/com/projetos/contactslist/db/SubscriberRepository.kt
@@ -0,0 +1,18 @@
+package com.projetos.contactslist.db
+
+class SubscriberRepository(private val dao: SubscriberDAO) {
+
+    val subscribers = dao.getAllSubscribers()
+
+    suspend fun insert(subscriber: Subscriber){
+        dao.insertSubscriber(subscriber)
+    }
+
+    suspend fun update(subscriber: Subscriber){
+        dao.updateSubscriber(subscriber)
+    }
+
+    suspend fun delete(subscriber: Subscriber){
+        dao.deleteSubscriber(subscriber)
+    }
+}
